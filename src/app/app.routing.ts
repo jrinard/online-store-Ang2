f2d84c5ec64901bf36fb500cc30f7cf0aa40bbfb
joshua <jrinard@hotmail.com>
2017-01-21 14:27:14 -0800

    welcome route

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders }  from '@angular/core'; //provide our router to the rest of the application
+import { Routes, RouterModule } from '@angular/router';//help us render specific components when specific URLs are provided to the router.
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const appRoutes: Routes = [   { //master list of all available routes in our application.
+    path: '', //URL Segment
+    component: WelcomeComponent //the component that should be rendered when the user navigates to this route.
+  }
+];
+
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);//forRoot() method is passed into routing. Routing is simply a special type of module that includes something called providers to help make information (like our routes) available to the rest of the application
